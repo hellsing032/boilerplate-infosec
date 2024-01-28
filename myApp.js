140502c8a134f4985a280e1e727d6cd035452f47
hellsing032 <saidina.065121083@unpak.ac.id>
2024-01-28 14:31:27 +0700

    fix

--- myApp.js
+++ myApp.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
 
-app.use(helmet())
 const app = express();
+app.use(helmet());
 
 app.get("/", (req, res) => {
   res.send("Hello world!");

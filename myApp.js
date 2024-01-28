3008792e5a763dedba92b41242dc43650e32b472
hellsing032 <saidina.065121083@unpak.ac.id>
2024-01-28 15:31:22 +0700

    feat: add helmet property

--- myApp.js
+++ myApp.js
@@ -1,68 +1,36 @@
-const express = require('express');
-const helmet = require('helmet');
-
+const express = require("express");
+const helmet = require("helmet");
+const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
 const app = express();
 app.use(helmet());
-
+app.use(helmet.hidePoweredBy({ setTo: "PHP 4.2.0" }));
+app.use(helmet.frameguard({ action: "deny" }));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    styleSrc: ['style.com'],
+    scriptSrc: ["'self'", "trusted-cdn.com"]
+  }
+}));
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
-app.listen(8080);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
+const api = require("./server.js");
+app.use(express.static("public"));
+app.disable("strict-transport-security");
+app.use("/_api", api);
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + "/views/index.html");
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+  console.log(`Your app is listening on http://localhost:${port}`);
 });
